caa7cefb99a2a2782c00a86d18af1b573ba4bd7b
ebaytheway <lafleurr4141@gmail.com>
2023-03-31 22:42:43 +0300

    feat:add todo data

---
+++ src/datatodo.jsx
@@ -0,0 +1,14 @@
+const data = [
+    {
+        todoName:"Do Homework",
+        isCompleted:true
+    },
+    {
+        todoName:"Write a Code",
+        isCompleted:false
+    },
+
+]
+
+
+export default data;
