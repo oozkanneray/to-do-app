17221529fe7f81308f13ce2d1e2638df14c9e677
ebaytheway <lafleurr4141@gmail.com>
2023-04-02 02:03:07 +0300

    feat: add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import {useState } from "react"
 import Todos from "./components/todos"
 
 function App() {
@@ -6,6 +6,19 @@ function App() {
   const [todo,setTodo] = useState("")
   const [allTodos,setAllTodos] = useState([])
   const [theme,setTheme] = useState("light")
+
+
+
+  const changeTheme = () => {
+
+    setTheme(theme == "light" ? "dark" : "light")
+    if(theme == "light") {
+      document.body.classList.add("dark")
+    }
+    else {document.body.classList.remove("dark")
+  }
+  }
+
 
 
   const handleInput = (e) => {
@@ -38,14 +51,14 @@ function App() {
 
 
   return (
-    <div className="flex flex-col item-center justify-center text-center">
-      <button onClick={() => {setTheme(theme == "light" ? "dark" : "light")}} className="absolute top-5 right-10"> Change Theme: {theme} </button>
-      <h1 className="text-5xl m-10 text-indigo-400" >To Do List</h1>
+    <div className="flex flex-col item-center text-center h-[100%] dark:bg-black">
+      <button onClick={changeTheme} className="absolute top-5 right-10 dark:text-white"> Change Theme: {theme} </button>
+      <h1 className="text-5xl m-10 text-indigo-400 dark:text-gray-300" >To Do List</h1>
       <div className="flex flex-row justify-center items-center m-5">
         <input
         onChange={handleInput}
         value={todo}
-        className="mr-5 border-2 w-100 h-10 border-indigo-800 rounded-lg bg-transparent text-center placeholder:text-center"
+        className="mr-5 border-2 w-100 h-10 border-indigo-800 rounded-lg bg-transparent text-center placeholder:text-center dark:text-white dark:border-gray-300"
         placeholder="Add a Todo"
         onKeyDown={(e) => {
           if(e.key == "Enter") addTodo()
@@ -53,7 +66,7 @@ function App() {
         ></input>
         <button 
         onClick={addTodo}
-        className=" bg-indigo-900 border-slate-700 text-white w-16 h-10 rounded-lg border-2 ml-5" >Add</button>
+        className=" bg-indigo-900 border-slate-700 text-white w-16 h-10 rounded-lg border-2 ml-5 dark:bg-gray-600 dark:border-gray-300" >Add</button>
       </div>
       <div className="flex flex-col justify-center items-center">
         {allTodos.map(item => <Todos key={item.id} deleteTodo={deleteTodo} allTodos={allTodos} handleMark={handleMark} setAllTodos={setAllTodos} item={item}/>)}
--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -1,6 +1,6 @@
 function Todos(props) {
   return (
-    <div className="flex flex-row justify-around w-[85%] items-center text-center w-96 border-2 border-indigo-400 rounded-lg m-5 bg-secondColor">
+    <div className="flex flex-row justify-around w-[35%] items-center text-center border-2 border-indigo-400 rounded-lg m-5 bg-secondColor dark:bg-gray-600 dark:border-gray-300">
       <div
       
         onClick={() => {
@@ -8,14 +8,14 @@ function Todos(props) {
         }}
         className={
           props.item.isCompleted
-            ? " w-[50%] text-2xl p-2 line-through select-none decoration-4"
-            : " w-[50%] text-2xl p-2 font-semibold select-none "
+            ? " w-[50%] text-2xl p-2 line-through select-none decoration-4 dark:text-white"
+            : " w-[50%] text-2xl p-2 font-semibold select-none dark:text-white"
         }
       >
         {props.item.todoName}
       </div>
       <button
-      className="w-[50%]"
+      className="w-[50%] dark:text-white"
       onClick={()=>{props.deleteTodo(props.allTodos.findIndex(item => item.todoName == props.item.todoName))}}
       >X</button>
     </div>
